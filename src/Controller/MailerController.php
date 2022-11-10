e4733fac331a042f0222b1c7705955d1a49b55c4
iivo <s7o0rm@gmail.com>
2022-11-10 20:04:25 +0100

    user created

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,30 +16,46 @@ class MailerController extends AbstractController
     /**
      * @Route("/mail", name="app_mailer")
      */
-    public function sendEmail(MailerInterface $mailer)
+
+
+    public function sendEmail(MailerInterface $mailer, Request $request)
     {
 
-        $table = 'table1';
-        $text = 'FOR THE HORDE';
-        $email = (new TemplatedEmail())
-            ->from('table1@menukarte.wip')
-            ->to('kelner@menukarte.wip')
-            ->subject('something')
+        $emailForm = $this->createFormBuilder()
+            ->add('message', TextareaType::class, ['attr' => array('rows' => 5)])
+            ->add('submit', SubmitType::class)
+            ->getForm();
 
-            ->htmlTemplate('mailer/mail.html.twig')
-            ->context([
-                'table' =>  $table,
-                'text' => $text
+        $emailForm->handleRequest($request);
+
+        if ($emailForm->isSubmitted()) {
+
+            $input = $emailForm->getData();
+            $text = $input['message'];
+
+            $table = 'table1';
+            $text = 'FOR THE HORDE';
+            $email = (new TemplatedEmail())
+                ->from('table1@menukarte.wip')
+                ->to('kelner@menukarte.wip')
+                ->subject('Message')
+                ->htmlTemplate('mailer/mail.html.twig')
+                ->context([
+                    'table' => $table,
+                    'text' => $text
+                ]);
+
+            $mailer->send($email);
+
+            $this->addFlash('Message', 'Message was send.');
+
+            return $this->redirect($this->generateUrl('app_mailer'));
+        }
+
+        return $this->render('mailer/index.html.twig',
+            [
+                'emailForm' => $emailForm->createView()
             ]);
-
-        $mailer->send($email);
-
-
-        return new Response('Email send.');
-
-//        return $this->render('mailer/index.html.twig', [
-//            'controller_name' => 'MailerController',
-//        ]);
 
     }
 }
